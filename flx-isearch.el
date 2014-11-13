afbcbac0e3c48304c22784c9ad7420625f7dce02
PythonNut <PythonNut@users.noreply.github.com>
2014-11-13 18:15:42 +0000

    Add documentation

--- flx-isearch.el
+++ flx-isearch.el
@@ -20,19 +20,35 @@
   :group 'flex-isearch)
 
 ;; flx-isearch has to store a lot of state
-(defvar flx-search-index 0)
+(defvar flx-isearch-index 0
+  "The index of the current symbol being searched for")
 
-(defvar flx-isearch-index 0)
-(defvar flx-isearch-point 0)
-(defvar flx-isearch-last-search "")
-(defvar flx-isearch-was-wrapped nil)
+(defvar flx-isearch-point 0
+  "The (point) at which the search started")
 
-(defvar flx-isearch-lazy-flag nil)
-(defvar flx-isearch-last-lazy-flag nil)
-(defvar flx-isearch-lazy-index nil)
-(defvar flx-isearch-lazy-point nil)
+(defvar flx-isearch-last-search ""
+  "The previous search string")
 
-(defvar flx-isearch-original-search-fun nil)
+(defvar flx-isearch-was-wrapped nil
+  "Whether or not the last searched caused the current search to wrap")
+
+(defvar flx-isearch-lazy-flag nil
+  "Whether or not the current searches are of the lazy variety")
+(defvar flx-isearch-last-lazy-flag nil
+  "Whether or not the last search was of the lazy variety")
+
+(defvar flx-isearch-lazy-index nil
+  "Holds the last value of `flx-isearch-index' during lazy
+highlighting")
+
+(defvar flx-isearch-lazy-point nil
+  "Holds the last value of `flx-isearch-point' during lazy
+highlighting")
+
+(defvar flx-isearch-original-search-fun nil
+  "Stores the previous value of `isearch-search-fun-function'
+during flx-isearch searches")
+
 (defvar flx-isearch-activated nil)
 
 
@@ -72,7 +88,11 @@
     result))
 
 (defun flx-isearch-sort (str symbols &optional cache)
+  "Sort the strings in `symbols' according to their `flx-score'
+with respect to `str'"
   (sort (cl-remove-if-not
+          ;; the duplicate call to flx-score has negligible
+          ;; overhead if a cache is used.
           (lambda (item) (car (flx-score (car item) str cache)))
           symbols)
     (lambda (a b)
@@ -81,17 +101,28 @@
         (or (car (flx-score (car b) str cache)) -1)))))
 
 ;; and the cache stuff
-(defvar flx-isearch-cache-level-1 nil)
-(defvar flx-isearch-cache-level-2 nil)
-(defvar flx-isearch-cache-level-3 nil)
+(defvar flx-isearch-cache-level-1 nil
+  "Used to store the list of symbols in the current buffer, so
+the buffer is not scanned on every search")
+
+(defvar flx-isearch-cache-level-2 nil
+  "Used to store the flx cache which speeds up flx's own internals")
+
+(defvar flx-isearch-cache-level-3 nil
+  "Used to store the complete list of sorted symbols so they are not
+recomputed on `isearch-repeat-forward' and `isearch-repeat-backward'")
 
 (defun flx-isearch-heatmap (symbol-name)
+  "The flx heatmap used to store the symbols in the current buffer"
   (flx-get-heatmap-str symbol-name))
 
 (defun flx-isearch-make-cache()
+  "The flx cache used to store the symbols in the current buffer"
   (flx-make-string-cache 'flx-isearch-heatmap))
 
 (defun flx-isearch-initialize-state ()
+  "Reset all stateful variables to their default values
+should be called before any search is started"
   (setq
     flx-isearch-cache-level-1 (hash-table-to-alist
                                 (flx-isearch-fuse
@@ -106,23 +137,31 @@
     flx-isearch-last-search ""))
 
 (defun flx-isearch-resolve-last-state ()
+  "Resolves changes of isearch state including switches from
+and to lazy highlighting and isearch wrapping"
+  ;; when switching to lazy highlighting
   (when (and flx-isearch-lazy-flag
           (not flx-isearch-last-lazy-flag))
+    ;; stash the state that will be clobbered
     (setq
       flx-isearch-lazy-index flx-isearch-index
       flx-isearch-lazy-point flx-isearch-point)
+    ;; reset variables
     (setq
       flx-isearch-last-lazy-flag t
       flx-isearch-index 0
       flx-isearch-point (point)))
 
+  ;; when switching back from lazy highlighting
   (when (and (not flx-isearch-lazy-flag)
           flx-isearch-last-lazy-flag)
+    ;; restore the stashed state
     (setq
       flx-isearch-last-lazy-flag nil
       flx-isearch-index flx-isearch-lazy-index
       flx-isearch-point flx-isearch-lazy-point))
 
+  ;; also reset when isearch wraps
   (when (and isearch-wrapped (not flx-isearch-was-wrapped))
     (setq
       flx-isearch-was-wrapped t
@@ -130,6 +169,8 @@
       flx-isearch-index 0)))
 
 (defun flx-isearch-compute-matches (string)
+  "Returns the list of matching symbols, sorted by flx-score.
+Takes advantage of all caches"
   (if (equal string flx-isearch-last-search)
     flx-isearch-cache-level-3
     (progn
@@ -142,6 +183,8 @@
           flx-isearch-cache-level-2)))))
 
 (defun flx-search-forward (string &optional bound noerror count)
+  "Search forward just like `search-forward' but with flx matching.
+Note that this means that newpoint > oldpoint is not always true."
   (interactive "M")
   (flx-isearch-resolve-last-state)
   (let* ((matches (flx-isearch-compute-matches string))
@@ -161,6 +204,8 @@
             (flx-search-forward string bound noerror count)))))))
 
 (defun flx-search-backward (string &optional bound noerror count)
+  "Search backward just like `search-backward' but with flx matching.
+Note that this means that newpoint < oldpoint is not always true."
   (interactive "M")
   (flx-isearch-resolve-last-state)
   (let* ((matches (flx-isearch-compute-matches string))
@@ -180,6 +225,8 @@
             (flx-search-backward string bound noerror count)))))))
 
 (defadvice isearch-lazy-highlight-search (around flx-isearch-set-lazy-flag)
+  "Sets a flag so flx-isearch can determine if the current search is
+of the lazy variety"
   (let ((flx-isearch-lazy-flag t))
     ad-do-it))
 
@@ -227,6 +274,7 @@ enabled."
       (ad-activate 'isearch-lazy-highlight-search))))
 
 (defun flx-isearch-forward (&optional regexp-p no-recursive-edit)
+  "Start a fuzzy forward isearch"
   (interactive "P\np")
   (when (and flx-isearch-mode
           (null regexp-p))
@@ -234,6 +282,7 @@ enabled."
   (isearch-mode t (not (null regexp-p)) nil (not no-recursive-edit)))
 
 (defun flx-isearch-backward (&optional regexp-p no-recursive-edit)
+  "Start a fuzzy backward isearch"
   (interactive "P\np")
   (when (and flx-isearch-mode
           (null regexp-p))
