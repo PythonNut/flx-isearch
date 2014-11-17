f92280afad29f68be8d9786e07058a056e402834
PythonNut <PythonNut@users.noreply.github.com>
2014-11-17 00:57:50 +0000

    `...` (markdown) ⟹ `...' (emacs docs)

--- flx-isearch.el
+++ flx-isearch.el
@@ -27,18 +27,18 @@
 
 ;; Fuzzy matching is awesome, when done right.
 
-;; This program lets you isearch for `fiis` and be taken to matches for `flx-isearch-initialize-state`.
+;; This program lets you isearch for "fiis" and be taken to matches for `flx-isearch-initialize-state'.
 ;; Searches currently only apply to symbols. The input string is flex matched to all symbols in the buffer,
 ;; and all matching symbols are searched one by one.
 
-;; For example, searching for `fii` in `flx-isearch.el` first takes you to
-;; * all instances of `flx-isearch-index` one by one
-;; * all instances of `flx-isearch-initialize-state` one by one
-;; * all instances of `flx-isearch-lazy-index` one by one
+;; For example, searching for `fii` in `flx-isearch.el' first takes you to
+;; * all instances of `flx-isearch-index' one by one
+;; * all instances of `flx-isearch-initialize-state' one by one
+;; * all instances of `flx-isearch-lazy-index' one by one
 ;; * [...]
-;; * all instances of `bounds-of-thing-at-point` one by one
+;; * all instances of `bounds-of-thing-at-point' one by one
 
-;; The _hope_ is that `flx` will be smart enough to quickly take you to the symbol you're thinking of
+;; The _hope_ is that `flx' will be smart enough to quickly take you to the symbol you're thinking of
 ;; with minimal effort.
 
 ;;; Code:
